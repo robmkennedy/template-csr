dbe2fb83924c9986b70fad1afe9038103f79bb26
Rob Kennedy <robkennedy@outlook.ie>
2025-08-05 18:37:06 +0100

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    base: '/template-csr/',
     test: {
         globals: true,
         environment: 'jsdom',
